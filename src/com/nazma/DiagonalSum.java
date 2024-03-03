b548b6680088cf76ef277020e616dd597a1bebe0
nazma98 <nazmasarker1311@gmail.com>
2024-03-03 21:53:18 +0600

    leetcode 1572 diagonal sum solution

---
+++ src/com/nazma/DiagonalSum.java
@@ -0,0 +1,22 @@
+package com.nazma;
+
+public class DiagonalSum {
+    public static void main(String[] args) {
+        int[][] mat = {{1,2,3}, {4,5,6},{7,8,9}};
+        System.out.println(diagonalSum(mat));
+    }
+
+    public static int diagonalSum(int[][] mat) {
+        int sum = 0;
+
+        for(int i=0;  i < mat.length; i++) {
+            for( int j = 0; j< mat.length; j++) {
+                if(i == j || i+j == mat.length -1) {
+                    sum += mat[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+}
