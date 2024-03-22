d0fc6285583ed1928bb6ad7e1191827340c863cb
nazma98 <nazmasarker1311@gmail.com>
2024-03-22 09:38:16 +0600

    leetcode 2148 next greatest small letter solution

---
+++ src/com/nazma/NextGreatestLetter.java
@@ -0,0 +1,28 @@
+package com.nazma;
+
+public class NextGreatestLetter {
+    public static void main(String[] args) {
+//        char[] letters = {'c', 'f', 'j'};
+        char target = 'b';
+        char[] letters = {'x', 'x', 'y', 'y'};
+        char ans = nextGreatestLetter(letters, target);
+        System.out.println(ans);
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+        }
+        return letters[start % letters.length];
+
+    }
+}
